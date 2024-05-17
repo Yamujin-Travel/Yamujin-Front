afc004e6669aed186298a4429d0e469108b4f786
박세은 <seeunbag30@gmail.com>
2024-05-17 20:57:09 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style scoped>
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="text-xl font-bold text-red-500">Home</h1>
+  </div>
+</template>
+
+<script lang="ts"></script>
