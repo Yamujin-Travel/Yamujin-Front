cf3c372d7d50c94abe24741591a62d6cee3fbaa1
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-23 00:53:20 +0900

    feat: navbar 디자인 및 카테고리 모달 기능 추가

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -1,26 +1,51 @@
 <script setup lang="ts">
+import CategoryModal from '@/modal/CategoryModal.vue';
+import { ref } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import { useUserStore } from '@/store/modules/users';
 
 const userStore = useUserStore();
 const router = useRouter();
 const route = useRoute();
+const isModalOpen = ref(false);
 
+const toggleModalOpenStatus = () => {
+  isModalOpen.value = !isModalOpen.value;
+};
 const routeToLogin = () => {
   router.push({ name: 'login' });
 };
 </script>
 
 <template>
+  <CategoryModal v-if="isModalOpen" v-model:isModalOpen="isModalOpen" />
   <div v-if="route.path !== '/login'" class="w-full h-[7rem] flex items-center justify-between px-[34px]">
     <div class="flex items-center gap-8">
       <RouterLink :to="{ name: 'home' }">
         <icon name="nav-logo" />
       </RouterLink>
-      <div class="flex h-6 gap-8 text-sm font-normal">
-        <RouterLink :to="{ name: 'recommend' }"> 서비스 </RouterLink>
-        <RouterLink :to="{ name: 'profile' }"> 마이페이지 </RouterLink>
-        <RouterLink :to="{ name: 'PostList', query: { page: 1 } }"> 커뮤니티 </RouterLink>
+      <div class="flex gap-8 text-sm font-normal">
+        <button class="relative flex items-center gap-2" @click="toggleModalOpenStatus">
+          <span :class="isModalOpen ? 'font-bold' : ''">서비스</span>
+
+          <icon
+            name="arrow"
+            style="transition: transform 0.2s"
+            :style="{ transform: isModalOpen ? 'rotate(180deg)' : 'rotate(0)' }"
+          />
+        </button>
+        <RouterLink
+          :to="{ name: 'profile' }"
+          :class="route.path.startsWith('/profile') ? 'font-bold border-b border-black' : ''"
+        >
+          마이페이지
+        </RouterLink>
+        <RouterLink
+          :to="{ name: 'PostList', query: { page: 1 } }"
+          :class="route.path.startsWith('/posts') ? 'font-bold border-b border-black' : ''"
+        >
+          커뮤니티
+        </RouterLink>
       </div>
     </div>
     <button
---
+++ src/modal/CategoryModal.vue
@@ -0,0 +1,69 @@
+<template>
+  <teleport to="#modal-root">
+    <div ref="modalRef">
+      <icon name="nav-modal-triangle" class="absolute z-[9999] top-[68px] left-[157px] nav-modal-triangle" />
+      <div
+        class="flex justify-center items-center absolute z-[9998] top-[78px] left-[119px] border-[1px] border-[#E0E0E0] w-[234px] h-[290px] rounded-[5px] shadow-category bg-white"
+      >
+        <div className="flex flex-col justify-between h-[13.875rem] w-[12.625rem]">
+          <button
+            class="flex items-center w-full h-10 gap-2 pl-10 hover:bg-background-200"
+            @click="routeTo('recommend')"
+          >
+            <icon name="recommend" />
+            <span class="text-xs font-bold">금융 상품 추천받기</span>
+          </button>
+          <button class="flex items-center w-full h-10 gap-2 pl-10 hover:bg-background-200" @click="routeTo('compare')">
+            <icon name="compare" />
+            <span class="text-xs font-bold">예적금 금리 비교하기</span>
+          </button>
+          <button
+            class="flex items-center w-full h-10 gap-2 pl-10 hover:bg-background-200"
+            @click="routeTo('exchange')"
+          >
+            <icon name="exchange" />
+            <span class="text-xs font-bold">환율 계산하기</span></button
+          ><button class="flex items-center w-full h-10 gap-2 pl-10 hover:bg-background-200" @click="routeTo('search')">
+            <icon name="search" />
+            <span class="text-xs font-bold">주변 은행 찾아보기 </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const modalRef = ref(null);
+
+const emit = defineEmits(['update:isModalOpen']);
+
+const routeTo = (routeName: string) => {
+  router.push({ name: routeName });
+  emit('update:isModalOpen', false);
+};
+
+const handleClickOutside = (event: MouseEvent) => {
+  if (
+    modalRef.value &&
+    (modalRef.value as HTMLElement).contains &&
+    !(modalRef.value as HTMLElement).contains(event.target as Node)
+  ) {
+    emit('update:isModalOpen', false);
+  }
+};
+
+onMounted(() => {
+  setTimeout(() => {
+    document.addEventListener('click', handleClickOutside);
+  }, 0);
+});
+
+onUnmounted(() => {
+  document.removeEventListener('click', handleClickOutside);
+});
+</script>
