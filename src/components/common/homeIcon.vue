57cf900d50102246c569a90973ae05878ff5a11e
박세은 <seeunbag30@gmail.com>
2024-05-22 17:54:35 +0900

    Feat: HomeView에서 사용할 svg 불러오는 컴포넌트 추가

---
+++ src/components/common/homeIcon.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const homeIcon = defineAsyncComponent(() => import(`../../assets/svg/home/${props.name}.svg`));
+</script>
+
+<template>
+  <component :is="homeIcon" />
+</template>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import store from '@/store';
 import router from './router';
 import App from '@/App.vue';
 import icon from '@/components/common/icon.vue';
+import homeIcon from '@/components/common/homeIcon.vue';
 import Navbar from '@/components/common/Navbar.vue';
 import './index.css';
 
@@ -13,5 +14,6 @@ store.use(piniaPluginPersistedstate);
 const app = createApp(App);
 
 app.component('icon', icon);
+app.component('homeIcon', homeIcon);
 app.component('navbar', Navbar);
 app.use(store).use(router).mount('#app');
