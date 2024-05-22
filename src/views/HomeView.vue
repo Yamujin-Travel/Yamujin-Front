1173f5ce1e93990cca31daaeb9dc016de2e2d066
박세은 <seeunbag30@gmail.com>
2024-05-23 00:50:47 +0900

    Design: homeView에 sample 이미지 넣기

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full h-[calc(100vh-7rem)] px-[7.4375rem]">
-    <div class="flex flex-col pt-28">
+  <div class="w-full h-[calc(100vh-7rem)] pl-[7.4375rem] flex">
+    <div class="flex flex-col mr-4 pt-28">
       <h1 class="mb-8 text-5xl font-bold leading-relaxed text-left">
         <div class="leading-snug"><span class="text-airbnb">야무진</span> 여행을 위한<br />금융 추천 서비스<br /></div>
         Yamujin Travel !
@@ -9,10 +9,15 @@
         야무야무야무얌무야무야무야무<br />야무야무야무 무무무무뭄 야무!! 야무야무야무야무뀨뀨<br />무야무야무야무야진
         여행!
       </p>
-      <RouterLink :to="{ name: 'recommend' }" class="w-[8rem]">
-        <button class="py-3 font-medium bg-airbnb text-background-100 w-[8rem] rounded-3xl">추천 받기</button>
+      <RouterLink :to="{ name: 'recommend' }" class="w-36">
+        <button
+          class="py-3 font-medium transition border-2 rounded-full w-36 bg-background-100 text-airbnb border-airbnb hover:bg-airbnb hover:text-background-100"
+        >
+          추천 받기
+        </button>
       </RouterLink>
     </div>
+    <homeIcon name="sample" />
   </div>
 </template>
 
