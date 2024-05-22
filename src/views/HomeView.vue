d66af59433aada38adbe0f72307e39e25e2bfe5f
박세은 <seeunbag30@gmail.com>
2024-05-22 17:03:03 +0900

    Design: 서비스 title 부분 UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <h1 class="text-xl font-bold text-red-500">Home 페이지입니다.</h1>
+  <div class="w-full h-[calc(100vh-7rem)] px-[7.4375rem]">
+    <div class="flex flex-col pt-28">
+      <h1 class="mb-8 text-5xl font-bold leading-relaxed text-left">
+        <div class="leading-snug"><span class="text-airbnb">야무진</span> 여행을 위한<br />금융 추천 서비스<br /></div>
+        Yamujin Travel !
+      </h1>
+      <p class="mb-8 text-xl text-left text-semiBlack">
+        야무야무야무얌무야무야무야무<br />야무야무야무 무무무무뭄 야무!! 야무야무야무야무뀨뀨<br />무야무야무야무야진
+        여행!
+      </p>
+      <button class="py-3 font-medium bg-airbnb text-background-100 w-[8rem] rounded-3xl">추천 받기</button>
+    </div>
   </div>
 </template>
 
