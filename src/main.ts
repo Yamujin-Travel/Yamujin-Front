9941b657131e9431cb42a958459abc92e4fa1f49
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-22 12:07:06 +0900

    chore: pinia-plugin-persistedstate 연결

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import router from './router';
 import App from '@/App.vue';
 import icon from '@/components/common/icon.vue';
 import './index.css';
 
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
 const app = createApp(App);
 
 app.component('icon', icon);
