c81ab61b2606aa2e2844b7d58a172d88d510f54c
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-24 04:06:10 +0900

    feat: 게시판 페이지 기능 구현

---
+++ src/components/GoToBack.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const props = defineProps({
+  goName: Object,
+});
+
+const clickBack = function () {
+  // router.go(-1)
+  if (props.goName) {
+    router.push(props.goName);
+  }
+};
+</script>
+
+<template>
+  <div>
+    <v-btn class="mb-10" rounded="xl" variant="plain" @click="clickBack">
+      <v-icon class="mx-0" start icon="mdi-arrow-left"></v-icon>
+      Back
+    </v-btn>
+  </div>
+</template>
+
+<style scoped>
+.mb-10 {
+  left: -15px;
+}
+</style>
